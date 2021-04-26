84505db8031caca69c227180f4845dabca1495da
Andrê Reges <dreeh.silva@hotmail.com>
2021-04-25 22:39:29 -0300

    Recap

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ import unittest
 
 class Test(unittest.TestCase):
     def setUp(self):
-        # cria uma instância do unittest, precisa do nome "setUp"
+        # Cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
 
-        # envia uma requisicao GET para a URL
+        # Envia uma requisição GET para a URL
         self.result = self.app.get('/')
                                                                                                                                                    
     def test_requisicao(self):
-        # compara o status da requisicao (precisa ser igual a 200)
+        # Compara o status da requisição (precisa ser igual a 200)
         self.assertEqual(self.result.status_code, 200)
 
     def test_conteudo(self):
-        # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
+        # Verifica o retorno do conteúdo da página
+        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps - Andre")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
